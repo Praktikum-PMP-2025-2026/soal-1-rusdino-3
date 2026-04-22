0728c48398bf6a544a35bde03bdc673061bf105f
Rusydi Noor Abdurrahman <71594097+rusdino@users.noreply.github.com>
2026-04-22 10:12:17 +0700

    fix: penggantian seluruh prototipe dan fungsi external dengan tipe void

--- soal1.c
+++ soal1.c
@@ -4,41 +4,47 @@
 #include <math.h>
 
 
-int sum(int *x, int n){
+void sum(int *x, int n, int* sum){
    int temp = 0;
    for(int i = 0; i <n; i++){
       temp += *(x+i);
    }
-   return temp;
+   *sum = temp;
 }
 
-int max(int *x, int n){
+void max(int *x, int n, int* max){
    int temp = -9999999;
    for(int i = 0; i <n; i++){
       if(*(x+i)>temp){
          temp = *(x+i);
       }
    }
-   return temp;
+   *max = temp;
 }
 
-float avg(int *x, int n){
-   float y =(float)sum(x,n)/n;
-   return y;
+void avg(int *x, int n, float* avg){
+   int sumx;
+   sum(x,n, &sumx);
+   *avg =(float)sumx/n;
 }
 
-int IDX(int *x, int n){
+void IDX(int *x, int n, int* idx){
+   int maxx;
+   max(x, n, &maxx);
    for(int i = 0; i <n; i++){
-      if(*(x+i)== max(x, n)){
-         return i;
+      if(*(x+i)== maxx){
+         *idx = i;
+         return;
       }
    }
 }
 
 int main(){
-   char buff[100]; 
+   char buff[100];
+   int sumx, maxx, idxx;
+   float avgx; 
    fgets(buff, sizeof(buff), stdin);
-   int N = (int)(strtok(buff, " ")[0]-'0');
+   int N = atoi((strtok(buff, " ")));
    if(N!=0){
       int x[N];
       for(int i =0; i < N; i++){
@@ -47,10 +53,14 @@ int main(){
             x[i] = atoi(temp);
    }
 }
-      printf("\nSUM %d", sum(x, N));
-      printf("\nAVG %0.2f", avg(x, N));
-      printf("\nMAX %d", max(x, N));
-      printf("\nIDX %d", IDX(x, N));
+   sum(x, N, &sumx);
+   avg(x, N, &avgx);
+   max(x, N, &maxx);
+   IDX(x, N, &idxx);
+   printf("\nSUM %d", sumx);
+   printf("\nAVG %0.2f", avgx);
+   printf("\nMAX %d", maxx);
+   printf("\nIDX %d", idxx);
    }
 
 }
